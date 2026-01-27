48e05e08d109a68cbf485fb1e84a77ff159f9eb7
Gleb Iudin <smartgleb@gmail.com>
2026-01-27 15:38:37 +0300

    SPH-47 refactor: внесены изменение в компонент кнопки + удален package-lock.json

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -4,14 +4,11 @@ import { clsx } from "@/shared/lib/clsx";
 import type { ButtonProps } from "./types";
 import styles from "./Button.module.scss";
 
-export const Button = ({ children, onClick, className, disabled }: ButtonProps) => {
+export const Button = (props: ButtonProps) => {
+  const { children, onClick, className, disabled } = props;
+  const buttonClassName = clsx(styles.button, className);
   return (
-    <button
-      type="button"
-      onClick={onClick}
-      className={clsx(styles.button, className)}
-      disabled={disabled}
-    >
+    <button type="button" onClick={onClick} className={buttonClassName} disabled={disabled}>
       {children}
     </button>
   );
