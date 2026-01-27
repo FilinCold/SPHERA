e964e9edff7e437bca8ea9ce903e39789a0de004
Gleb Iudin <smartgleb@gmail.com>
2026-01-27 21:37:02 +0300

    SPH-47-v2 feat: добавлена масштабируемая кнопка

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { clsx } from "@/shared/lib/clsx";
-import type { ButtonProps } from "./types";
+
 import styles from "./Button.module.scss";
+
+import type { ButtonProps } from "./types";
 
 export const Button = (props: ButtonProps) => {
   const { children, onClick, className, disabled } = props;
   const buttonClassName = clsx(styles.button, className);
+
   return (
     <button type="button" onClick={onClick} className={buttonClassName} disabled={disabled}>
       {children}
