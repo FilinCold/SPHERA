117245de395b2c0be4e2d3b2b04b6cccb1c19a67
Gleb Iudin <smartgleb@gmail.com>
2026-01-28 15:42:10 +0300

    SPH-48-v2 feat: добавлена возможность внедрять customStyles извне

--- src/shared/ui/AdaptiveBlock/AdaptiveBlock.tsx
+++ src/shared/ui/AdaptiveBlock/AdaptiveBlock.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import clsx from "clsx";
+
 import styles from "./AdaptiveBlock.module.scss";
 
 import type { AdaptiveBlockProps } from "./types";
 
-export const AdaptiveBlock: React.FC<AdaptiveBlockProps> = ({ children }) => {
-  return <div className={styles.adaptiveBlock}>{children}</div>;
+export const AdaptiveBlock = ({ children, className }: AdaptiveBlockProps) => {
+  const blockClassName = clsx(styles.adaptiveBlock, className);
+
+  return <div className={blockClassName}>{children}</div>;
 };
--- src/shared/ui/AdaptiveBlock/types.ts
+++ src/shared/ui/AdaptiveBlock/types.ts
@@ -1,3 +1,4 @@
 export interface AdaptiveBlockProps {
   children: React.ReactNode;
+  className?: string;
 }
