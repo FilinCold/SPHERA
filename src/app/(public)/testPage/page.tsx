1973c97e19427ee368b1512d9fdf53744fe3d030
Gleb Iudin <smartgleb@gmail.com>
2026-01-30 01:46:10 +0300

    SPH-58 feat: добавлено модальное окно и тестовая страница

---
+++ src/app/(public)/testPage/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useState } from "react";
+
+import { Modal } from "@/shared/components/Modal/Modal";
+import { Button } from "@/shared/ui/Button";
+
+export default function TestPage() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>Открыть модальное окно</Button>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <div>
+          <p>content1</p>
+          <p>content2</p>
+          <p>content3</p>
+        </div>
+      </Modal>
+    </>
+  );
+}
