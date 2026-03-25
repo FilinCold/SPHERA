312be6eef6ffd3873a6b14e71c73436ccaa19f90
sotn9plyur <samuraichaga@gmail.com>
2026-03-25 12:46:43 +0300

    Добавлены компоненты

---
+++ src/shared/components/SearchBar/types.ts
@@ -0,0 +1,28 @@
+export interface Option {
+  label: string;
+  value: string;
+}
+
+export interface Breadcrumb {
+  label: string;
+  href?: string;
+}
+
+export interface SearchParams {
+  query: string;
+  shop?: string;
+  organization?: string;
+}
+
+export interface SearchBarProps {
+  title?: string;
+  breadcrumbs?: Breadcrumb[];
+  totalCount?: number;
+
+  searchPlaceholder?: string;
+
+  onSearch: (params: SearchParams) => void;
+
+  shopOptions?: Option[];
+  organizationOptions?: Option[];
+}
