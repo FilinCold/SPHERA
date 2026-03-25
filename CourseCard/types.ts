437aa0ac0aef85ee269c362316a61f66ee2c9f02
Andrew Chaga <121227680+sotn9plyur@users.noreply.github.com>
2026-03-25 16:14:45 +0300

    Add files via upload

---
+++ CourseCard/types.ts
@@ -0,0 +1,12 @@
+export type CourseStatus = "active" | "archived" | "deleted";
+
+export interface CourseCardProps {
+  title: string;
+  description: string;
+  image: string;
+  status: CourseStatus;
+  usersCount: number;
+  date: string;
+  link: string;
+  users: UserAvatar[];
+}
